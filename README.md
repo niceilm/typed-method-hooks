03384e4865b3507442ce0c085930a9756ba434a9
flynn <niceilm@naver.com>
2017-02-02 11:23:50 +0900

    init files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Typed method-hooks
+
+The type definition for [method-hooks](https://github.com/leonidez/method-hooks).
+
+## License
+
+MIT
---
+++ index.d.ts
@@ -0,0 +1,20 @@
+/**
+ * @link https://github.com/leonidez/method-hooks
+ */
+
+interface BeforeHook {
+  (methodArgs: any, methodOptions: ValidatedMethodOption): any;
+}
+
+interface AfterHook {
+  (methodArgs: any, returnValue: any, methodOptions: ValidatedMethodOption): any;
+}
+
+export interface ValidatedMethodOption {
+  beforeHooks?: BeforeHook[];
+  afterHooks?: AfterHook[];
+}
+
+declare module 'meteor/lacosta:method-hooks' {
+  export let MethodHooks: any;
+}
